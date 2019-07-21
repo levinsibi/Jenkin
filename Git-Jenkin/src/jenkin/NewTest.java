9a4fb9877b2909ce53778316277cea6f333a26f8
LEVIS <levinsibi@gmail.com>
2019-07-21 22:40:37 +0530

    ss

--- Git-Jenkin/src/jenkin/NewTest.java
+++ Git-Jenkin/src/jenkin/NewTest.java
@@ -13,7 +13,7 @@ public static void test1()
 WebDriver driver=new FirefoxDriver();
 driver.get("https://www.facebook.com/");
 driver.manage().window().maximize();
-driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sachoo@b.com");
+driver.findElement(By.xpath("//input[@id='email']")).sendKeys("sachooii@b.com");
 driver.findElement(By.xpath("//input[@id='pass']")).sendKeys("pass1");
 System.out.println(driver.getTitle());
 }
